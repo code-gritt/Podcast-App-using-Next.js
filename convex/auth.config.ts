7fa8f38d343e6cda4cd98520a403d9d4bfac88b6
Gokul <gokulchandan24@gmail.com>
2024-06-05 22:40:22 +0530

    login signup added

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://supreme-baboon-99.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
